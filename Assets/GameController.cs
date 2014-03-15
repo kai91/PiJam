14898b062aa401b633ffed79ebd5dad352e5fbc9
kai91 <wngkai.91@gmail.com>
2014-03-15 00:10:53 -0700

    first commit

---
+++ Assets/GameController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject timer;
+    public bool start;
+
+	// Use this for initialization
+	void Start () {
+        start = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/ScaleFont.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ScaleFont : MonoBehaviour
+{
+    public Vector2 offset;
+    public float ratio = 10;
+
+    void OnGUI()
+    {
+        float finalSize = (float)Screen.width / ratio;
+        guiText.fontSize = (int)finalSize;
+        guiText.pixelOffset = new Vector2(offset.x * Screen.width, offset.y * Screen.height);
+    }
+
+}
