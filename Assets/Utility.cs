8e8e0c45b64e2767e85b10789c22ae35c1a54538
whiteSkar <sypower07@hotmail.com>
2014-03-15 00:32:49 -0700

    Added Utility class with the random range generator function

---
+++ Assets/Utility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets
+{
+    public static class Utility
+    {
+        public static KeyValuePair<int, int> getFruitRangePair(int lowLimitOfStartValue, int highLimitOfStartValue, int desiredRange)
+        {
+            var random = new Random((int)DateTime.Now.Ticks);
+
+            var lowValue = (int)(random.NextDouble() * (highLimitOfStartValue - lowLimitOfStartValue)) + lowLimitOfStartValue;
+            var highValue = lowValue + desiredRange;
+
+            return new KeyValuePair<int, int>(lowValue, highValue);
+        }
+    }
+}
