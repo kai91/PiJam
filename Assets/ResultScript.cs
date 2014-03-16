51d631583c20b19de02dec79758c12de8be5a0d5
kai91 <wngkai.91@gmail.com>
2014-03-16 10:57:29 -0700

    Added result to scene

---
+++ Assets/ResultScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResultScript : MonoBehaviour {
+
+    public GameObject few, many, nice;
+
+	// Use this for initialization
+	void Start () {
+        few.renderer.enabled = false;
+        many.renderer.enabled = false;
+        nice.renderer.enabled = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
