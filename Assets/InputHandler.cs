56391d4f68593b109413d4efc0610dea5388f29d
whiteSkar <sypower07@hotmail.com>
2014-03-15 17:02:38 -0700

    Added Input Handler which handles touch for the three ingredients

---
+++ Assets/InputHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputHandler : MonoBehaviour
+{
+    GameObject[] ingredients;
+    // Use this for initialization
+    void Start()
+    {
+        ingredients = GameObject.FindGameObjectsWithTag("Ingredient");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                Vector3 touchPosition3 = Camera.main.ScreenToWorldPoint(touch.position);
+                //Debug.Log("Touch position: " + touch.position);
+                Vector2 touchPosition2 = new Vector2(touchPosition3.x, touchPosition3.y);
+
+                foreach (var ingredient in ingredients)
+                {
+                    if (Physics2D.OverlapPoint(touchPosition2) == ingredient.collider2D)
+                    {
+                        Debug.Log(ingredient.name);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/TemporaryBlueberryScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TemporaryBlueberryScript : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        foreach (var touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                Vector3 touchPosition3 = Camera.main.ScreenToWorldPoint(touch.position);
+                Debug.Log("Touch position: " + touch.position);
+                Vector2 touchPosition2 = new Vector2(touchPosition3.x, touchPosition3.y);
+                if (collider2D == Physics2D.OverlapPoint(touchPosition2))
+                {
+                    Debug.Log("hit");
+                }
+            }
+        }
+	}
+
+    // Testing purpose
+    void OnMouseDown()
+    {
+        Debug.Log("Mouse Down");
+    }
+}
