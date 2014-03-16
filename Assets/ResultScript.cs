ae5ff1eacff02be9f7b79549357fe8c846f0453d
kai91 <wngkai.91@gmail.com>
2014-03-16 11:31:16 -0700

    Updated ResultScript

--- Assets/ResultScript.cs
+++ Assets/ResultScript.cs
@@ -7,14 +7,25 @@ public class ResultScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        few.renderer.enabled = false;
-        many.renderer.enabled = false;
-        nice.renderer.enabled = false;
-	
+        hideResult();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    public void hideResult()
+    {
+        few.renderer.enabled = false;
+        many.renderer.enabled = false;
+        nice.renderer.enabled = false;
+    }
+
+    public void display(int mode)
+    {
+        if (mode == 1) few.renderer.enabled = true;
+        if (mode == 2) many.renderer.enabled = true;
+        if (mode == 3) nice.renderer.enabled = true;
+    }
 }
